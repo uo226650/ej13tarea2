3b864a0cc55184a5a12d26cf22b55b2c35f343da
uo226650 <uo226650@uniovi.es>
2020-11-24 13:49:00 +0100

    repo creado

---
+++ MapaGeoJSON.js
@@ -0,0 +1,71 @@
+/*
+API de Google Maps que cargue desde la máquina cliente un archivo en formato GeoJSON 
+con la situación del inicio y de los hitos de las rutas turísticas 
+construidas en la práctica de XML*/
+
+//AIzaSyC8aUp4J8B-MOmk6mg4A8cSsbE3qzEpB1g
+
+class MapaDinamico{
+
+  initMap(){
+      this.map = new google.maps.Map(document.getElementById('map'), {
+      center: new google.maps.LatLng(43.362407 , -6.159651),
+      zoom: 10,
+      mapTypeId: 'terrain'
+    });
+  }
+
+  /**
+   * Pinta las rutas sobre el mapa usando la clase PolyLine
+   */
+  initDrawing(){
+
+    var puntos = []; //Crea un array de elementos LatLng
+    //Formato coordenada geoJSON es (lng,lat): -6.069794,43.389283,55 
+    this.coordinates.forEach(coordinate => {
+      puntos.push(new google.maps.LatLng(coordinate[1], coordinate[0])) //Cogemos el primer y segundo elemento que corresponden a longitud y latitud respectivamente
+    });
+    
+    var polyLinea = new google.maps.Polyline({
+      path: puntos,
+      map: this.map,
+      strokeColor: 'ff0000ff',
+      strokeWeight: 4,
+  });
+  }
+
+  /**
+   * Recorre las coordenadas de las rutas a
+   * partir de las propiedades del objeto JSON
+   * 
+   * @param {File} archivo en formato geoJSON para ser procesado
+   */
+  procesaGeo(archivo){
+    this.coordinates = [];
+    var archivoJSON = JSON.parse(archivo);
+    archivoJSON.features.forEach( feature => {
+      feature.geometry.coordinates.forEach(coordinate => {
+        this.coordinates.push(coordinate);
+      });
+    });
+    this.initDrawing();
+  }
+
+  /**
+   * Carga el archivo desde la máquina de cliente
+   * Lee su contenido y lo envía como parámetro al 
+   * método procesaKML
+   * 
+   * @param {*} files 
+   */
+  leerArchivoGeo(files){ 
+    var archivo = files[0];
+    var lector = new FileReader();
+    lector.onload = function (evento) {
+      this.procesaGeo(lector.result);
+    }.bind(this);
+    lector.readAsText(archivo);
+  }
+}
+
+var mapaDinamicoGoogle = new MapaDinamico();

ced7630c79cd85eef589c56c5ed8c069144a6937
uo226650 <uo226650@uniovi.es>
2020-11-28 21:19:08 +0100

    file format exception handling

--- MapaGeoJSON.js
+++ MapaGeoJSON.js
@@ -5,11 +5,11 @@ construidas en la práctica de XML*/
 
 //AIzaSyC8aUp4J8B-MOmk6mg4A8cSsbE3qzEpB1g
 
-class MapaDinamico{
+class MapaDinamico {
 
-  initMap(){
-      this.map = new google.maps.Map(document.getElementById('map'), {
-      center: new google.maps.LatLng(43.362407 , -6.159651),
+  initMap() {
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: new google.maps.LatLng(43.362407, -6.159651),
       zoom: 10,
       mapTypeId: 'terrain'
     });
@@ -18,20 +18,20 @@ class MapaDinamico{
   /**
    * Pinta las rutas sobre el mapa usando la clase PolyLine
    */
-  initDrawing(){
+  initDrawing() {
 
     var puntos = []; //Crea un array de elementos LatLng
     //Formato coordenada geoJSON es (lng,lat): -6.069794,43.389283,55 
     this.coordinates.forEach(coordinate => {
       puntos.push(new google.maps.LatLng(coordinate[1], coordinate[0])) //Cogemos el primer y segundo elemento que corresponden a longitud y latitud respectivamente
     });
-    
+
     var polyLinea = new google.maps.Polyline({
       path: puntos,
       map: this.map,
       strokeColor: 'ff0000ff',
       strokeWeight: 4,
-  });
+    });
   }
 
   /**
@@ -40,15 +40,30 @@ class MapaDinamico{
    * 
    * @param {File} archivo en formato geoJSON para ser procesado
    */
-  procesaGeo(archivo){
+  procesaGeo(archivo) {
     this.coordinates = [];
-    var archivoJSON = JSON.parse(archivo);
-    archivoJSON.features.forEach( feature => {
-      feature.geometry.coordinates.forEach(coordinate => {
-        this.coordinates.push(coordinate);
+    try {
+
+      var archivoJSON = JSON.parse(archivo);
+      archivoJSON.features.forEach(feature => {
+        feature.geometry.coordinates.forEach(coordinate => {
+          this.coordinates.push(coordinate);
+        });
       });
-    });
-    this.initDrawing();
+      this.initDrawing();
+      //Limpia el campo "error" en caso de que exista
+      if (document.getElementsByClassName("error").length > 0)//Si está presente el mensaje de error
+        document.getElementsByClassName("error")[0].remove();
+
+    } catch (error) {
+
+      var errorElem = document.createElement("p");
+      errorElem.setAttribute("class", "error");
+      errorElem.innerText = "El archivo no está en formato geojson y no se puede representar";
+      document.getElementById("map").before(errorElem);
+      
+    }
+
   }
 
   /**
@@ -58,7 +73,7 @@ class MapaDinamico{
    * 
    * @param {*} files 
    */
-  leerArchivoGeo(files){ 
+  leerArchivoGeo(files) {
     var archivo = files[0];
     var lector = new FileReader();
     lector.onload = function (evento) {
